bca489115512e1fcd1dc0ba1d93455854f6a7e36
Daniel Buompane <danielbuompane@gmail.com>
2016-02-17 18:18:44 -0500

    get department salary

--- employee_review_test.rb
+++ employee_review_test.rb
@@ -36,76 +36,76 @@ class EmployeeReviews < Minitest::Test
     assert_equal [new_employee], a.employees
   end
 
-  # def test_can_get_employee_name
-  #   new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
-  #   assert_equal "Dan", new_employee.name
-  # end
-  #
-  # def test_can_get_employee_salary
-  #   new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
-  #   assert_equal 50000.00, new_employee.salary
-  # end
-  #
-  # def test_can_get_a_department_name
-  #   a = Department.new(name: "Marketing")
-  #   assert_equal "Marketing", a.name
-  # end
-  #
-  # def test_total_department_salary
-  #   a = Department.new(name: "Marketing")
-  #   new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
-  #   old_employee = Employee.new(name: "Yvonne", email: "Yvonne@urFired.com", phone: "919-123-4567", salary: 40000.00)
-  #   assert a.add_employee(new_employee)
-  #   assert a.add_employee(old_employee)
-  #   assert_equal 90000.00, a.department_salary
-  # end
-  #
-  # def test_add_employee_review
-  #   xavier = Employee.new(name: "Xavier", email: "ProfX@marvel.com", phone: "911", salary: 70000.00)
-  #   assert xavier.add_employee_review(positive_review_one)
-  # end
-  #
-  # def test_set_employee_performance
-  #   new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
-  #   old_employee = Employee.new(name: "Yvonne", email: "Yvonne@urFired.com", phone: "919-123-4567", salary: 4000.00)
-  #   new_employee.set_employee_performance(true)
-  #   old_employee.set_employee_performance(false)
-  #   assert new_employee.satisfactory
-  #   refute old_employee.satisfactory
-  # end
-  #
-  # def test_give_raise_by_percent
-  #   new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
-  #   assert_equal 54000, new_employee.raise_by_percent(0.08)
-  # end
-  #
-  # def test_give_raise_by_amount
-  #   new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
-  #   assert_equal 60000, new_employee.raise_by_amount(10000)
-  # end
-  #
-  # def test_department_raises_based_on_criteria
-  #   a = Department.new(name: "Marketing")
-  #   xavier = Employee.new(name: "Xavier", email: "ProfX@marvel.com", phone: "911", salary: 70000.00)
-  #   new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
-  #   old_employee = Employee.new(name: "Yvonne", email: "Yvonne@urFired.com", phone: "919-123-4567", salary: 40000.00)
-  #   a.add_employee(xavier)
-  #   a.add_employee(new_employee)
-  #   a.add_employee(old_employee)
-  #   xavier.set_employee_performance(true)
-  #   new_employee.set_employee_performance(true)
-  #   old_employee.set_employee_performance(false)
-  #   a.department_raise(14000.00) {|e| e.satisfactory == true && e.salary < 60000.00}
-  #   assert_equal 70000.00, xavier.salary
-  #   assert_equal 64000.00, new_employee.salary
-  #   assert_equal 40000.00, old_employee.salary
-  # end
-  #
-  # def test_evaluate_employee_review
-  #   xavier = Employee.new(name: 'Xavier', email: 'ProfX@marvel.com', phone: '911', salary: 70000.00)
-  #   xavier.add_employee_review(positive_review_one)
-  #   assert xavier.satisfactory
-  # end
+  def test_can_get_employee_name
+    new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
+    assert_equal "Dan", new_employee.name
+  end
+
+  def test_can_get_employee_salary
+    new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
+    assert_equal 50000.00, new_employee.salary
+  end
+
+  def test_can_get_a_department_name
+    a = Department.new(name: "Marketing")
+    assert_equal "Marketing", a.name
+  end
+
+  def test_total_department_salary
+    a = Department.new(name: "Marketing")
+    new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
+    old_employee = Employee.new(name: "Yvonne", email: "Yvonne@urFired.com", phone: "919-123-4567", salary: 40000.00)
+    assert a.employees << new_employee
+    assert a.employees << old_employee
+    assert_equal 90000.00, a.department_salary
+  end
+
+  def test_add_employee_review
+    xavier = Employee.new(name: "Xavier", email: "ProfX@marvel.com", phone: "911", salary: 70000.00)
+    assert xavier.add_employee_review(positive_review_one)
+  end
+
+  def test_set_employee_performance
+    new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
+    old_employee = Employee.new(name: "Yvonne", email: "Yvonne@urFired.com", phone: "919-123-4567", salary: 4000.00)
+    new_employee.set_employee_performance(true)
+    old_employee.set_employee_performance(false)
+    assert new_employee.satisfactory
+    refute old_employee.satisfactory
+  end
+
+  def test_give_raise_by_percent
+    new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
+    assert_equal 54000, new_employee.raise_by_percent(0.08)
+  end
+
+  def test_give_raise_by_amount
+    new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
+    assert_equal 60000, new_employee.raise_by_amount(10000)
+  end
+
+  def test_department_raises_based_on_criteria
+    a = Department.new(name: "Marketing")
+    xavier = Employee.new(name: "Xavier", email: "ProfX@marvel.com", phone: "911", salary: 70000.00)
+    new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
+    old_employee = Employee.new(name: "Yvonne", email: "Yvonne@urFired.com", phone: "919-123-4567", salary: 40000.00)
+    a.add_employee(xavier)
+    a.add_employee(new_employee)
+    a.add_employee(old_employee)
+    xavier.set_employee_performance(true)
+    new_employee.set_employee_performance(true)
+    old_employee.set_employee_performance(false)
+    a.department_raise(14000.00) {|e| e.satisfactory == true && e.salary < 60000.00}
+    assert_equal 70000.00, xavier.salary
+    assert_equal 64000.00, new_employee.salary
+    assert_equal 40000.00, old_employee.salary
+  end
+
+  def test_evaluate_employee_review
+    xavier = Employee.new(name: 'Xavier', email: 'ProfX@marvel.com', phone: '911', salary: 70000.00)
+    xavier.add_employee_review(positive_review_one)
+    assert xavier.satisfactory
+  end
 
   private def negative_review_one
     "Zeke is a very positive person and encourages those around him, but he has not done well technically this year. There are two areas in which Zeke has room for improvement. First, when communicating verbally (and sometimes in writing), he has a tendency to use more words than are required. This conversational style does put people at ease, which is valuable, but it often makes the meaning difficult to isolate, and can cause confusion. Second, when discussing new requirements with project managers, less of the information is retained by Zeke long-term than is expected. This has a few negative consequences: 1) time is spent developing features that are not useful and need to be re-run, 2) bugs are introduced in the code and not caught because the tests lack the same information, and 3) clients are told that certain features are complete when they are inadequate. This communication limitation could be the fault of project management, but given that other developers appear to retain more information, this is worth discussing further."
